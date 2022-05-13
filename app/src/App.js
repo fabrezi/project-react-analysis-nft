7331edfb7c74cff0a3717df21abc075bc3b96223
unknown <fkschon@gmail.com>
2022-05-13 15:30:12 -0400

    hardcode img

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
-
+import crypto from 'C:\\Users\\paul\\vscode-projects\\project-react-analysis-nft\\app\\src\\pictures\\crypto1.PNG'
+import ape from 'C:\\Users\\paul\\vscode-projects\\project-react-analysis-nft\\app\\src\\pictures\\ape02.PNG'
+import ape1 from 'C:\\Users\\paul\\vscode-projects\\project-react-analysis-nft\\app\\src\\pictures\\ape03.PNG'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Gutenberg Project</p>
+        <p>NFT--ANALYSIS</p>
           <table className="App-table">
             <tr>
-              <th>id</th>
-              <th>num_sales</th>
+              <th>ID </th>
+              <th>name</th>
             </tr>
             <tr>
-              <td>	158831</td>
-              <td>3</td>
+              <td>123881 </td>
+              <td>Jhonny</td>
             </tr>
           </table>
+          <img src={crypto} alt="" />
+          <img src={ape} alt="image not found"/>
+          <img src={ape1} alt="" />
       </header>
     </div>
   );
